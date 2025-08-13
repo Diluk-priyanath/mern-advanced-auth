525c35e09164646e24112594b048a7c4c56d87f1
Diluk-priyanathh <dilukpriyanath26@gmail.com>
2025-08-13 12:51:56 +0530

    project compleated

---
+++ backend/db/connectDB.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error('MongoDB connection error:', error.message);
+        process.exit(1);
+    }
+};
+
+// Export the connectDB function to be used in other files
+export default connectDB;
